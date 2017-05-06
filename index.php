ef5b3faf9e45875f24e19e52751045edb88386ba
moaleryani <moaleryani@gmail.com>
2017-05-06 15:18:33 +0200

    Implementation of basic quiz functionality

--- index.php
+++ index.php
@@ -19,26 +19,69 @@
       <a class="navbar-brand" href="#">Euro Quiz</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="/index.php">Quiz Time</a></li>
-      <li><a href="/browse.php">Browse</a></li>
-      <li><a href="/profileview.php">Random</a></li>
+      <li class="active"><a href="./index.php">Quiz Time</a></li>
+      <li><a href="./browse.php">Browse</a></li>
+      <li><a href="./profileview.php">Random</a></li>
     </ul>
   </div>
 </nav>
+       <?php
+// Read JSON file
+$json = file_get_contents('./eurovision-mock-db.json');
 
+//Decode JSON
+$json_data = json_decode($json,true);
+$one_item = $json_data[rand(0, count($json_data) - 1)];
+$one_item2 = $json_data[rand(0, count($json_data) - 1)];    
+while ($one_item==$one_item2) {
+   $one_item2 = $json_data[rand(0, count($json_data) - 1)];  
+}
+$one_item_string= json_encode($one_item['PerformerImage']);
+$one_item2_string= json_encode($one_item2['PerformerImage']);    
+?>     
+<script>
+function dialog(title, text, newid, script_include)
+{   
+    document.body.innerHTML += '<script src="' + script_include + '"><\/script>';
+    document.body.innerHTML += '<div id="' + newid + '"  title="' + title + '"><p>' + text + '</p></div>';
+}
+
+</script>
+    
+<script>
+function myFunction(performer1, performer2) {
+   
+    if ( parseInt(performer1) > parseInt(performer2)){
+  document.getElementById("Changethis").innerHTML = 'You have answered correctly';}
+        else{
+              document.getElementById("Changethis").innerHTML = 'Wrong answers';
+        }
+
+}
+</script>
+    
     <div class="well text-center">
             <div class="col-lg-12">
-                <h1 class="page-header">Who got the most votes?</h1>
+                <h1 id="Changethis"class="page-header">Who got the most votes?</h1>
             </div>    <div class="row">
         <div class="col-md-6">
-            <a href="#"><img src="http://www.eurovisioncovers.co.uk/ui16.jpg" width="400" height="400" alt="imagenotfound.jpg"></a>
+            <a href="./index.php"><img src=<?php echo $one_item_string; ?> width="400" height="400" alt="imagenotfound.jpg"  class="img-circle"></a>
+             <h3 class="media-heading"><?php echo $one_item['Performer'];  ?><small> <?php echo $one_item['Country'];  ?></small></h3>
+             <div> <span><strong>Song: </strong></span>
+                        <span class="label label-info"><?php echo $one_item['Song'];  ?></span></div>
+            <button onclick="myFunction('<?php echo $one_item['Points']; ?>','<?php echo $one_item2['Points'];  ?>')"><?php echo $one_item['Performer'];  ?> has more points</button>
         </div>
         <div class="col-md-6">
-            <a href="#"><img src="http://www.eurovisioncovers.co.uk/au16.jpg" width="400" height="400" alt="imagenotfound.jpg"></a>
+            <a href="./index.php"><img src=<?php echo $one_item2_string; ?> width="400" height="400" alt="imagenotfound.jpg"  class="img-circle"></a>
+             <h3 class="media-heading"><?php echo $one_item2['Performer'];  ?><small> <?php echo $one_item2['Country'];  ?></small></h3>
+             <div> <span><strong>Song: </strong></span>
+                        <span class="label label-info"><?php echo $one_item2['Song'];  ?></span></div>
+            <button onclick="myFunction('<?php echo $one_item2['Points']; ?>','<?php echo $one_item['Points'];  ?>')"><?php echo $one_item2['Performer'];  ?> has more points</button>
+           
+
         </div>
     </div>
 </div>
-
 </body>
 </html>
 
