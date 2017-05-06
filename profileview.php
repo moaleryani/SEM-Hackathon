9a30ae944fcd09acca9e526922bef6d8d43c4a4b
moaleryani <moaleryani@gmail.com>
2017-05-06 11:33:08 +0200

    Introduced profile view

---
+++ profileview.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Euro Quiz</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Euro Quiz</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Quiz Time</a></li>
+      <li><a href="#browse.php">Browse</a></li>
+      <li><a href="#">Random</a></li>
+    </ul>
+  </div>
+</nav>
+
+    <div class="well text-center">
+    <div class="row">
+        <div>
+            <a href="#"><img src="http://www.eurovisioncovers.co.uk/ui16.jpg" width="400" height="400" alt="imagenotfound.jpg"  class="img-circle"></a>
+            
+                    
+        </div>
+        <h3 class="media-heading">Performer Name <small> Country</small></h3>
+        <div> <span><strong>Song: </strong></span>
+                        <span class="label label-info">Name of Song</span></div>
+           <div> <span><strong>Total Points: </strong></span>
+                        <span class="label label-info">500</span></div>         
+             
+    </div>
+</div>
+
+</body>
+</html>
+
